bd21e0267d8f4f00ed8109a6017aa035068df171
marcel <marceljmachado@.gmail.com>
2022-07-15 16:00:04 -0300

    pet

---
+++ pet-shop-tutorial/src/js/app.js
@@ -0,0 +1,129 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function () {
+    // Load pets.
+    $.getJSON('../pets.json', function (data) {
+      var petsRow = $('#petsRow');
+      var petTemplate = $('#petTemplate');
+
+      for (i = 0; i < data.length; i++) {
+        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-age').text(data[i].age);
+        petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+        petsRow.append(petTemplate.html());
+      }
+    });
+
+    return await App.initWeb3();
+  },
+
+  initWeb3: async function () {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });;
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
+  },
+
+  initContract: function () {
+    $.getJSON('Adoption.json', function (data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      // Artifacts are information about our contract such as its deployed address and Application Binary Interface (ABI). The ABI is a JavaScript object defining how to interact with the contract including its variables, functions and their parameters.
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function () {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  markAdopted: function () {
+    // We first declare the variable adoptionInstance outside of the smart contract calls so we can access the instance after initially retrieving it.
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function (instance) {
+      adoptionInstance = instance;
+
+      // Using call() allows us to read data from the blockchain without having to send a full transaction, meaning we won't have to spend any ether.
+      return adoptionInstance.getAdopters.call();
+    }).then(function (adopters) {
+      // We then loop through all of them, checking to see if an address is stored for each pet. Since the array contains address types, Ethereum initializes the array with 16 empty addresses. This is why we check for an empty address string rather than null or other falsey value.
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          // Once a petId with a corresponding address is found, we disable its adopt button and change the button text to "Success", so the user gets some feedback.
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function (err) {
+      console.log(err.message);
+    });
+  },
+
+  handleAdopt: function (event) {
+    event.preventDefault();
+
+    var petId = parseInt($(event.target).data('id'));
+
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function (instance) {
+        adoptionInstance = instance;
+
+        // Execute adopt as a transaction by sending account
+        // Transactions require a "from" address and have an associated cost. This cost, paid in ether, is called gas. The gas cost is the fee for performing computation and/or storing data in a smart contract.
+        return adoptionInstance.adopt(petId, { from: account });
+      }).then(function (result) {
+        return App.markAdopted();
+      }).catch(function (err) {
+        console.log(err.message);
+      });
+    });
+
+  }
+
+};
+
+$(function () {
+  $(window).load(function () {
+    App.init();
+  });
+});
---
+++ truffle-project/README.md
@@ -0,0 +1,20 @@
+# Truffle
+
+## Install Truffle for Linux
+
+```
+npm install truffle -g
+```
+
+## Initialize
+
+```
+truffle init
+```
+
+## Commands
+
+```
+truffle compile
+truffle migrate
+```
